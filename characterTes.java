6f01c9087c83a2a688866e84cd7217654cee8da2
androidbeginnernepal <anujshrestha16@gmail.com>
2014-09-15 16:43:54 +0545

    characterTest for java

---
+++ characterTes.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anuj
+ */
+ class characterTest
+{
+    
+    public static void main(String[] args)    
+{
+    Character ch1=new Character('x');
+    Character ch2=new Character('y');
+    Character ch6=new Character('B');
+    Character ch7=Character.valueOf('1');
+    Character ch8=Character.valueOf('x');
+    int difference=ch1.compareTo(ch2);
+    if(difference==0)
+        System.out.print(ch1+"AND"+ch2+"are same");
+    else if(difference<0)
+        System.out.println(ch1+"is less than"+ch2);
+        else
+        System.out.println(ch1+"is less than"+ch2);
+    
+        System.out.println(ch1+"and"+ch8+"are same");
+    
+    
+    
+    if(d)
+}
+      }
+
+
+
